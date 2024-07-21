142792388cf3fcbd5dd5ac601e5e80aab9a3f4fa
ZhanabekZ <zhanabekzinekenov@gmail.com>
2024-07-21 12:07:28 +0500

    Initial Commit

---
+++ Weather_vk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_vk
+//
+//  Created by ZhZinekenov on 21.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
